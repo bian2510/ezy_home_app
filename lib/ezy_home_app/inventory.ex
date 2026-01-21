93c985585276a629a9d375c6ff64223de23df3ff
Fabian Hernandez <bian251091@gmail.com>
2026-01-20 21:46:17 -0300

    Created facade to connect everything

---
+++ lib/ezy_home_app/inventory.ex
@@ -0,0 +1,32 @@
+defmodule EzyHomeApp.Inventory do
+  @moduledoc """
+  Fachada del Inventario.
+  Este archivo es el único con el que hablará tu página web (LiveView).
+  """
+
+  # Llamamos a los módulos que creamos hace un momento
+  alias EzyHomeApp.Inventory.Products
+  alias EzyHomeApp.Inventory.Bundles
+
+  # =================================================================
+  # DELEGACIONES: Si piden "x", que lo haga el módulo correspondiente
+  # =================================================================
+
+  # --- PRODUCTOS ---
+  defdelegate list_products, to: Products, as: :list
+  defdelegate get_product!(id), to: Products, as: :get!
+  defdelegate create_product(attrs), to: Products, as: :create
+  defdelegate update_product(product, attrs), to: Products, as: :update
+  defdelegate delete_product(product), to: Products, as: :delete
+
+  # --- PACKS (BUNDLES) ---
+  defdelegate list_bundles, to: Bundles, as: :list
+  defdelegate get_bundle!(id), to: Bundles, as: :get!
+  defdelegate create_bundle(attrs), to: Bundles, as: :create
+  defdelegate create_bundle_item(attrs), to: Bundles, as: :create_item
+
+  # --- FUNCIONES ESPECIALES (Stock Virtual) ---
+  defdelegate calculate_bundle_stock(bundle), to: Bundles, as: :calculate_stock
+  defdelegate list_bundles_with_stock, to: Bundles, as: :list_with_virtual_stock
+  defdelegate sell_bundle(id, qty \\ 1), to: Bundles, as: :sell
+end
