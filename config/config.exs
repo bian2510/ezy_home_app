ca16c053339ded29f4014baacad7000096b024c0
Fabian Hernandez <bian251091@gmail.com>
2026-01-25 13:55:44 -0300

    Added authentication using mix

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :ezy_home_app, :scopes,
+  user: [
+    default: true,
+    module: EzyHomeApp.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: EzyHomeApp.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :ezy_home_app,
   ecto_repos: [EzyHomeApp.Repo],
   generators: [timestamp_type: :utc_datetime]
--- lib/ezy_home_app_web/router.ex
+++ lib/ezy_home_app_web/router.ex
@@ -1,6 +1,8 @@
 defmodule EzyHomeAppWeb.Router do
   use EzyHomeAppWeb, :router
 
+  import EzyHomeAppWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule EzyHomeAppWeb.Router do
     plug :put_root_layout, html: {EzyHomeAppWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -46,5 +49,33 @@ defmodule EzyHomeAppWeb.Router do
       live_dashboard "/dashboard", metrics: EzyHomeAppWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", EzyHomeAppWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{EzyHomeAppWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", EzyHomeAppWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{EzyHomeAppWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
